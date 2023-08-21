9f4ef9c07d926d2fee78759287ff76a8be1451fd
divah21 <divahsmart@gmail.com>
2023-08-22 01:28:39 +0200

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import SuperQuality from './SuperQuality';
+import Service from './Service';
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    SuperQuality,
+    Service,
+}
